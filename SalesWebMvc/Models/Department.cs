ded80c65a92a8a7c08b05ba214e5fb9e0649649e
Juan Pablo <prof.juanpablo@gmail.com>
2024-05-23 20:36:03 -0300

    Criação dos Models

---
+++ SalesWebMvc/Models/Department.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SalesWebMvc.Models
+{
+    public class Department
+    {
+     
+        public int DepartmentId { get; set; }
+        public string? Name { get; set; }
+        public ICollection<Seller>? Sellers { get; set; }
+    }
+}
---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,20 @@
+﻿using SalesWebMvc.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+
+    { 
+        public int SalesRecordId { get; set; }
+
+    [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+    public DateTime Date { get; set; }
+
+    [DisplayFormat(DataFormatString = "{0:F2}")]
+    public double Amount { get; set; }
+    public SaleStatus Status { get; set; }
+    public Seller? Seller { get; set; }
+
+}
+}
